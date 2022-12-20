7e6ac8aec1b4fee18bfaae33930f59c675d3a322
Gerónimo Basso <geronimobasso@MacBook-Pro-de-Geronimo.local>
2022-12-20 12:01:19 +0100

    finished session 5

---
+++ sesion-05-numbers/ejercicios-numbers.js
@@ -0,0 +1,10 @@
+let alturaCen = 200
+let alturaMet = 2.00
+let peso = 97.5
+
+let altura_red = Math.ceil(alturaCen)
+let peso_red = Math.floor(peso)
+
+let sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(sonIguales)
